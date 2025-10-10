1b2a806c68f77b3665f8aa7c08b8a5f10a3e872b
Savitskaya Sonya <ssavitskaya7@gmail.com>
2025-10-10 22:53:13 +0300

    added comments to circle.py

--- circle.py
+++ circle.py
@@ -2,9 +2,11 @@ import math
 
 
 def area(r):
+    '''Принимается радиус r, возвращает площадь круга'''
     return math.pi * r * r
 
 
 def perimeter(r):
+    '''Принимает радиус r, возвращает периметр круга'''
     return 2 * math.pi * r
 
